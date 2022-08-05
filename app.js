0a2d05d9b9c202afd84b84303962fd1a97a36c0f
Lukasz Nogaj <lukasznogaj@Lukaszs-Mac-mini.local>
2022-08-05 14:15:39 +0100

    5thaugust

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 // Creating the HTML elements , first div hero with menu and heading
 //First page before scroll
-// Select the main container
+// const gitIcon = <i class='fa-brands fa-github'></i>;
+// console.log(gitIcon);
+// Select the main container an  create the main divs
 const mainDiv = document.querySelector('#main-div');
+// Hero div
+const headerDiv = document.createElement('div');
+headerDiv.classList.add('header');
+mainDiv.appendChild(headerDiv);
+console.log(headerDiv);
 // Top menu
 const topMenu = document.createElement('ul'); // and add class and id
 topMenu.setAttribute('id', 'top-bar');
 topMenu.classList.add('top-menu');
-mainDiv.appendChild(topMenu);
+headerDiv.appendChild(topMenu);
 const menuItems = document.createElement('li');
 menuItems.setAttribute('id', 'menu-items');
 menuItems.classList.add('list-item');
+// Create the strapi logo
+const strapiLogo = document.createElement('h2');
+strapiLogo.classList.add('strapi-logo');
+strapiLogo.innerHTML = 'strapi';
+topMenu.appendChild(strapiLogo);
 
 // Try to create the menu with the for loop and array with menu text
 const menuArray = [
@@ -33,9 +45,41 @@ for (let i = 0; i < 4; i++) {
   menuArray.forEach((menuArr) => {
     menuItems.innerHTML = menuArray[i];
   });
-  // menuItems.innerHTML = menuArray[0]; // Is working for all 5
-  console.log(menuItems);
 }
 
-// Checking with the console if the element exist
-console.log(topMenu, mainDiv);
+// Create the three button git hub, get started and div for them
+// First button
+const headerBtnDiv = document.createElement('div');
+headerBtnDiv.classList.add('header-btn');
+topMenu.appendChild(headerBtnDiv);
+const gitHubBtn1 = document.createElement('button');
+gitHubBtn1.classList.add('gitHubBtn');
+headerBtnDiv.appendChild(gitHubBtn1);
+gitHubBtn1.innerHTML = '<i class=" fa-brands fa-github"></i> star';
+// add the github icon into button
+
+//Second buttonc with numbers
+const gitHubBtn2 = document.createElement('button');
+gitHubBtn2.classList.add('gitHubBtn');
+headerBtnDiv.appendChild(gitHubBtn2);
+gitHubBtn2.innerHTML = '298057';
+// Third button with the Get Started text
+const startedBtn = document.createElement('button');
+startedBtn.classList.add('getstarted-btn');
+startedBtn.innerHTML = 'Get Started';
+topMenu.appendChild(startedBtn);
+
+//  The div hero
+const heroDiv = document.createElement('div');
+heroDiv.classList.add('hero');
+mainDiv.appendChild(heroDiv);
+// The small heading with the span , a link learn more
+const newSpan = document.createElement('div');
+newSpan.classList.add('new-span');
+heroDiv.appendChild(newSpan);
+// Create the new look like button div
+const newSquare = document.createElement('div');
+newSquare.classList.add('new-square');
+newSquare.innerHTML = 'New';
+newSpan.appendChild(newSquare);
+console.log(heroDiv);
